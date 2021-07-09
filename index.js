55336038138c7ee4e9403e8e8a4b4e1f0ed5346b
Spatchy <spatchy@spatchy.net>
2021-07-09 11:04:04 +0100

    switch child_process out for node-pty

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const child_process = require('child_process');
+const pty = require('node-pty');
 const client = new Discord.Client();
-
 
 const credentials = JSON.parse(fs.readFileSync('./credentials.json'))
 
+var shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
+
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
-	const mc = child_process.spawn('docker', ['attach', 'mc']);
+
+	mc = pty.spawn(shell, [], {
+		name: 'xterm-color',
+		cols: 80,
+		rows: 30,
+		cwd: process.env.HOME,
+		env: process.env
+	});
+
 	mc.stdout.on('data', data => {
 		console.log(data);
 	});
 	mc.stderr.on('data', data => {
 		console.log(data.toString('utf-8'));
-	})
+	});
+
+	mc.write('docker attach mc');
 });
 
 client.on('message', msg => {
